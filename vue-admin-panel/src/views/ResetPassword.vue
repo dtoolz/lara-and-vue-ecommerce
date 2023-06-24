26ca1a700eb8cf6411888a40b9270ed8a5381c02
dtoolz <donaldaudriel@gmail.com>
2023-06-24 20:16:09 +0100

    added reset and request password pages

--- vue-admin-panel/src/views/ResetPassword.vue
+++ vue-admin-panel/src/views/ResetPassword.vue
@@ -3,7 +3,7 @@
       <div class="max-w-md w-full space-y-8">
         <div>
           <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Change Existing Password</h2>
           <p class="mt-2 text-center text-sm text-gray-600">
             Or
             {{ ' ' }}
@@ -14,21 +14,16 @@
           <input type="hidden" name="remember" value="true" />
           <div class="rounded-md shadow-sm space-y-3">
             <div>
-              <label for="email-address" class="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+              <label for="new-password" class="sr-only">New Password</label>
+              <input id="new-password" name="password" type="password"  required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="New Password" />
             </div>
             <div>
-              <label for="password" class="sr-only">Password</label>
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+              <label for="confirm-password" class="sr-only">Confirm New Password</label>
+              <input id="confirm-password" name="confirm_password" type="password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm New Password" />
             </div>
           </div>
   
           <div class="flex items-center justify-between">
-            <div class="flex items-center">
-              <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-              <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
-            </div>
-  
             <div class="text-sm">
               <router-link :to="{name: 'requestPassword'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </router-link>
             </div>
@@ -39,7 +34,7 @@
               <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                 <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
               </span>
-              Sign in
+              Update
             </button>
           </div>
         </form>
