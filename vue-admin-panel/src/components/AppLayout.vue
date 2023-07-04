0fa7913c6303a9f4b168939ffaaf0cec9880e5d1
dtoolz <donaldaudriel@gmail.com>
2023-07-04 16:02:35 +0100

    added working sidebar and navbar

---
+++ vue-admin-panel/src/components/AppLayout.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="min-h-full flex bg-gray-100">
+          <!--the sidebar-->
+          <Sidebar :class="{'-ml-[200px]': !sidebarOpened}"/>
+
+          <div class="flex-1">
+            <Navbar @toggle-sidebar="toggleSidebar"/>
+            <main class="p-6">
+               <router-view></router-view>
+            </main>
+          </div>
+          
+    </div>
+</template>
+    
+<script setup>
+    import {ref, onMounted, onUnmounted, } from 'vue';
+    import Sidebar from "./Sidebar.vue";
+    import Navbar from "./Navbar.vue";
+
+    const sidebarOpened = ref(true);
+
+    function toggleSidebar(){
+        sidebarOpened.value = !sidebarOpened.value;
+    }
+
+    onMounted(()=>{
+      handleOpenedSidebar();
+      window.addEventListener('resize', handleOpenedSidebar);
+    });
+
+    onUnmounted(()=>{
+       window.removeEventListener('resize', handleOpenedSidebar);
+    });
+
+    function handleOpenedSidebar(){
+        if(window.outerWidth <= 768) {
+          sidebarOpened.value = false;
+        } else {
+          sidebarOpened.value = true;
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
---
+++ vue-admin-panel/src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="w-[200px] transition-all bg-indigo-600 text-gray-300 py-4 px-2">
+       <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 px-3 rounded hover:bg-gray-600">
+           <span class="mr-2">
+            <HomeIcon class="w-5" />
+           </span>
+           <span>Dashboard</span>
+       </router-link>
+       <router-link :to="{name: 'app.products'}" class="flex items-center p-2 px-3 rounded hover:bg-gray-600">
+           <span class="mr-2">
+            <UsersIcon class="w-5" />
+           </span>
+           <span>Products</span>
+       </router-link>
+       <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 px-3 rounded hover:bg-gray-600">
+           <span class="mr-2">
+            <ViewListIcon class="w-5" />
+           </span>
+           <span>Users</span>
+       </router-link>
+       <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 px-3 rounded hover:bg-gray-600">
+           <span class="mr-2">
+            <ChartBarIcon class="w-5" />
+           </span>
+           <span>Reports</span>
+       </router-link>
+    </div>
+</template>
+    
+<script setup>
+    import {HomeIcon, UsersIcon, ViewListIcon, ChartBarIcon} from '@heroicons/vue/outline';
+</script>
+
+<style scoped>
+    
+</style>
