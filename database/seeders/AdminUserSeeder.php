497d513b53de4a1d37c094045c31e0b28c0a5676
dtoolz <donaldaudriel@gmail.com>
2023-07-08 14:59:17 +0100

    added an admin user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+           'name' => 'Admin',
+           'email' => 'Admin@example.com',
+           'email_verified_at' => now(),
+           'password' => bcrypt('admin1234'),
+           'is_admin' => true
+        ]);
+    }
+}
