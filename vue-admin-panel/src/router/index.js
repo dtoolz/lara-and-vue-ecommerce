532005ea25d710fd0cc910cf6a80b7bfcac3cd86
dtoolz <donaldaudriel@gmail.com>
2023-07-12 11:53:12 +0100

    admin login/logout working

--- vue-admin-panel/src/router/index.js
+++ vue-admin-panel/src/router/index.js
@@ -69,9 +69,8 @@ router.beforeEach((to, from, next)=>{
     if(to.meta.requiresAuth && !store.state.user.token){
         next({name: 'login'});
     } else if(to.meta.requiresGuest && store.state.user.token){
-        next({name: 'app/dashboard'});
-    }
-     else {
+        next({name: 'app.dashboard'});
+    } else {
         next();
     }
 });
